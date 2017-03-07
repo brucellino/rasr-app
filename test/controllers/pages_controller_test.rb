7fad5dbbaffb66e84f197bc97a6f5d6fd80b9d54
Bruce Becker <brucellino@gmail.com>
2017-03-07 10:50:08 +0100

    Update tests
      1. Remove the package installation in travis
      1. Add the ignore SSL bit in the initializer
      1. Add test for about page

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,5 +5,8 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get pages_home_url
     assert_response :success
   end
-
+  test "should get about" do
+    get pages_about_url
+    assert_response :success
+  end
 end
