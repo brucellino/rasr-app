6274ef23c62e576e915785a6d4cf8683afb1a722
Bruce Becker <brucellino@gmail.com>
2017-03-07 12:47:11 +0100

    remove api server resource and scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,5 @@ Rails.application.routes.draw do
 # Experiments
 
   get 'experiments/index'
-
-
-  resources :fgapi_servers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
