dd72ca3b8e9ac61ffb84c2b05f1ed1e177dd05d5
Bruce Becker <brucellino@gmail.com>
2017-03-07 14:52:09 +0100

    library_layout is ok

--- app/controllers/glibrary_controller.rb
+++ app/controllers/glibrary_controller.rb
@@ -1,9 +1,15 @@
 class GlibraryController < ApplicationController
   require "time"
-
+  def index
+    @repos = $repos.parsed_response
+    @repo_list = @repos.map { |repo| repo['name']}
+    #render json: @repos
+  end
   def time_remaining
-    
+
     return @glibrary_token_expires
 
+  end
+  def view
   end
 end
