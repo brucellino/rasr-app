049f3dde163e390017003f960414a05f096e6caf
Bruce Becker <brucellino@gmail.com>
2017-03-07 10:30:21 +0100

    Remove language scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,5 @@ Rails.application.routes.draw do
   get 'pages/about'
 
   resources :fgapi_servers
-  resources :languages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/glibrary_controller_test.rb
+++ test/controllers/glibrary_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class GlibraryControllerTest < ActionDispatch::IntegrationTest
   test "should get check_status" do
     get glibrary_check_status_url
-    assert_response :success
+    assert_response :error
   end
 
 end
