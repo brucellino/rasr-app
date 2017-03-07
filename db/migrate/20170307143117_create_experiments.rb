df45260cfa23e626b234201da62a11d2f789b8ff
Bruce Becker <brucellino@gmail.com>
2017-03-07 16:32:03 +0100

    Add language model

--- db/migrate/20170307143117_create_experiments.rb
+++ db/migrate/20170307143117_create_experiments.rb
@@ -1,6 +1,10 @@
 class CreateExperiments < ActiveRecord::Migration[5.0]
   def change
     create_table :experiments do |t|
+      t.string :user
+      t.string :language
+      t.boolean :primary
+      t.string :repo
 
       t.timestamps
     end
