9e90e1ad0255d81b317d11b69061a3843110496d
Bruce Becker <brucellino@gmail.com>
2017-03-07 13:47:50 +0100

    Showing table of repos

--- app/controllers/glibrary_controller.rb
+++ app/controllers/glibrary_controller.rb
@@ -1,12 +1,9 @@
 class GlibraryController < ApplicationController
   require "time"
-  @glibrary_token_created = Time.iso8601($glibrary_response['created'])
-  @glibrary_ttl = $glibrary_response['ttl']
-  @glibrary_token_expires = @glibrary_token_created + @glibrary_ttl
-  puts" Glibrary token expires at #{@glibrary_token_expires}"
 
-  def repos
+  def time_remaining
     
+    return @glibrary_token_expires
 
   end
 end
